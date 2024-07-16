55d0368dd046a0efc35c0c1f810e8870173d2cf9
Oscapoco <okpoco15@gmail.com>
2024-07-16 09:27:18 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav>
+        <div className='circle-box'><div className='circle-1'></div></div>
+        <div className='list-box'>
+          <div className='list'>Home</div>
+          <div className='list'>About</div>
+          <div className='list'>Trade</div>
+          <div className='list'>Blog</div>
+          <div className='list'>Contact</div>
+        </div>
+        <div className='button-box'><button>BTN</button></div>
+      </nav>
+      <header>
+        <div className='left'>
+          <p>NEVER GIVE UP ON A DREAM JUST
+          BECAUSE OF THE TIME IT WILL <br/>TAKE
+          TO ACCOMPLISH IT. THE TIME WILL PASS ANYWAY</p>
+        </div>
+        <div className='right'>
+          <p>GO OUT INTO THE WORLD WITH YOUR PASSION<br/>
+          AND LOVE FOR WHAT YOU DO, AND JUST NEVER
+          GIVE UP</p>
+        </div>
       </header>
+      <main>
+        <div className='one'><p>REST IF YOU HAVE TO, TAKE A WALK <br/>IF YOURE TIRED BUT DONT GIVE UP</p>
+        </div>
+        <div className='one'>
+          <p>REMEMBER THAT EVERYTHING HAPPENS FOR<br/> A REASON, DON'T REGRET THE CHOICE YOU<br/> CHOSE BECAUSE IT IS THE BEST<br/> DECISION YOU'VE EVER MADE</p>
+        </div>
+        <div className='one'>
+          <p>WHATEVER DIFFICULTIES YOURE FACING<br/>YOU'LL WIN MAN</p>
+        </div>
+        <div className='one'>
+          <p>NEVER GIVE UP ON SOMETHING YOU BELIEVE IN </p>
+        </div>
+      </main>
+      <div className='content-1'>
+        <div className='left-1'>
+          <div className='top'><p>YOU NEVER GIVE UP, EVEN IF YOU SHOULD</p></div>
+          <div className='bottom'><p>WINNERS NEVER QUIT, AND QUITTERS NEVER WIN</p></div>
+        </div>
+        <div className='right-1'>
+          <div className='top'><p>ON EVERYTHING YOU DO, DON'T DISAPPOINT YOURSELF</p></div>
+          <div className='bottom'><p>THIS IS YOUR TIME TO SHINE</p></div>
+        </div>
+      </div>
+      <div className='content-2'>
+        <p> NEVER GIVE UP ON SOMETHING YOU CAN NEVER GO A <br/>DAY WITHOUT
+        THINKING ABOUT IT</p>
+      </div>
+      <footer>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+        <div className='circle'></div>
+      </footer>
     </div>
   );
 }
